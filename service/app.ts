e729ddd22672ad412ffa3ab398be384dc86d0e55
Markus <mkau@macbookpro.lan>
2021-02-28 18:29:01 +0200

    Add dummy endpoint for products

--- service/app.ts
+++ service/app.ts
@@ -1,4 +1,5 @@
-import express = require('express')
+import * as express from 'express'
+import {router} from './products/products'
 
 export const app = express()
 app.use(express.json())
@@ -6,3 +7,5 @@ app.use(express.json())
 app.get('/health', (_req: express.Request, res: express.Response) => {
   res.json({health: 'ok'}).end()
 })
+
+app.use('/products', router)
