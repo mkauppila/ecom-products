197d2e5c9340b57c64a94b8c5bb73e154efa2ad2
Markus Kauppila <markus.kauppila@gmail.com>
2021-02-28 18:18:37 +0200

    Fix typing for health endpoint

--- service/app.ts
+++ service/app.ts
@@ -3,6 +3,6 @@ import express = require('express')
 export const app = express()
 app.use(express.json())
 
-app.get('/health', (_req, res) => {
+app.get('/health', (_req: express.Request, res: express.Response) => {
   res.json({health: 'ok'}).end()
 })
